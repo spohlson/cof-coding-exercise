7a066dbd124e35e5b3826493cf22cf6ca7d826b6
spohlson <spohlson@gmail.com>
2018-05-30 18:00:43 -0700

    Removed unnecessary code

--- src/main/java/com/cof/app/App.java
+++ src/main/java/com/cof/app/App.java
@@ -2,9 +2,7 @@ package com.cof.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
 
-@Profile("!test")
 @SpringBootApplication
 public class App {
 
